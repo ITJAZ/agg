c97ae9c6cf81212d05eb6e35e6dbf29af09ab645
almonds <jyunan1995@gmail.com>
2024-09-26 21:35:52 +0800

    happy birthday

---
+++ src/main/java/io/almonds/agg/AggApplication.java
@@ -0,0 +1,13 @@
+package io.almonds.agg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggApplication.class, args);
+	}
+
+}
