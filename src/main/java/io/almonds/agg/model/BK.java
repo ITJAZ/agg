00dc4466720c42cf8f6c2b50f8ef964adce9022b
almonds <jyunan1995@gmail.com>
2024-09-27 21:55:46 +0800

    remove ID for keeping simple

--- src/main/java/io/almonds/agg/model/BK.java
+++ src/main/java/io/almonds/agg/model/BK.java
@@ -2,7 +2,6 @@ package io.almonds.agg.model;
 
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;
@@ -13,10 +12,8 @@ import lombok.Setter;
 @Setter
 public class BK {
 
-  @Id
-  private long id;
-
   private String blno;
 
   private List<String> cntrNo;
+
 }
