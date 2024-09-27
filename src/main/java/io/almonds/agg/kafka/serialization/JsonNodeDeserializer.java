d93c55c663c20f1866118fec7a97797a1f7e3b32
almonds <jyunan1995@gmail.com>
2024-09-27 21:52:36 +0800

    isolating object mapper

--- src/main/java/io/almonds/agg/kafka/serialization/JsonNodeDeserializer.java
+++ src/main/java/io/almonds/agg/kafka/serialization/JsonNodeDeserializer.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 import org.apache.kafka.common.serialization.Deserializer;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonNodeDeserializer implements Deserializer<JsonNode> {
+
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
   @Override
   public JsonNode deserialize(String topic, byte[] data) {
     try {
-      return DefaultJsonSerde.OBJECT_MAPPER.readTree(data);
+      return OBJECT_MAPPER.readTree(data);
     } catch (IOException e) {
       throw new RuntimeException(e.getMessage());
     }
